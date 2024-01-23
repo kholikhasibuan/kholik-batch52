ceaba1511dfdb171fb1b12d4312e5a808f7d3065
kholikhasibuan <kholikhasibuan004@gmail.com>
2024-01-24 00:40:20 +0700

    first commit

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const port = 5000
+const config = require('./src/config/config.json');
+const { Sequelize, QueryTypes} = require('sequelize')
+const sequelize =  new Sequelize(config.local)
+sequelize.authenticate()
+    .then(() => {
+        console.log('Koneksi ke database berhasil.');
+    })
+    .catch(err => {
+        console.error('Tidak dapat terhubung ke database:', err);
+    });
 
 app.set("view engine", "hbs")
 app.set("views", path.join(__dirname, 'src/views'))
@@ -11,15 +21,35 @@ app.use(express.urlencoded({ extended: false }))
 
 app.get('/', home)
 app.get('/contact', contact)
+app.post('/delete-card/:id', deleteCard)
+
 app.get('/add-project', addProjectView)
 app.post('/add-project', addProject)
-app.get('/detail', detail)
+
+app.get('/update-project/:id', updateProjectView)
+app.post('/update-project', updateProject)
+
+app.get('/detail/:id', detail)
 app.get('/testimonial', testimonials)
 
+const data = []
+async function home(req, res) {
+    try {
+        const query = 'SELECT * FROM personalweb';
+        const obj = await sequelize.query(query, { type: QueryTypes.SELECT });
+        console.log(obj)
 
-function home(req, res) {
-    res.render('index');
+        // Menyimpan data dari database ke array 'data'
+
+
+        res.render('index', { data: obj });
+    } catch (error) {
+        console.error('Error saat mengambil data dari database:', error);
+        res.status(500).send('Internal Server Error');
+    }
 }
+
+
 
 function contact(req, res) {
     res.render('contact')
@@ -31,25 +61,118 @@ function addProjectView(req, res) {
 
 function addProject(req, res) {
     const name = req.body.name
-    const startDate = req.body.startDate
-    const endDate = req.body.endDate
+    const start = req.body.startDate
+    const end = req.body.endDate
+    const startDate = new Date(req.body.startDate)
+    const endDate = new Date(req.body.endDate)
     const desc = req.body.desc
     const checkbox = req.body.checkbox
+    const checkboxes = ['nodeJs', 'nextJs', 'reactJs', 'typeScript'].filter(checkbox => req.body[checkbox])
     
-    console.log('Project Name :', name)
-    console.log('Start Date :', startDate)
-    console.log('End Date :', endDate)
-    console.log('Description :', desc)
-    console.log('Technologies :', checkbox)
+    // Hitung selisih dalam hari
+    const days = dayDifference(startDate , endDate)
+
+    // Menentukan unit durasi berdasarkan selisih waktu
+    const { duration, unit } = chooseDuration(days)
+
+    // Ambil icon yang dipilih
+    const icons = {
+        nodeJs: checkboxes.includes('nodeJs') ? "/assets/img/nodejs.png" : '',
+        nextJs: checkboxes.includes('nextJs') ? "/assets/img/nextjs.png" : '',
+        reactJs: checkboxes.includes('reactJs') ? "/assets/img/react.png"  : '',
+        typeScript: checkboxes.includes('typeScript') ? "/assets/img/typescirpt.png" : ''
+    }
+
+    const dataProject = {name, start, end, startDate, endDate, desc, checkboxes, duration, unit, icons}
+
+    data.unshift(dataProject)
+    res.redirect('/')
+}
+async function updateProjectView(req, res) {
+    const id = req.params.id
+    const dataFilter = await sequelize.query("select * from personalweb where author_id ="+ id,{type:QueryTypes.SELECT})
+   
+
+    res.render('updateproject', {data: dataFilter }) 
+}
+
+function updateProject(req, res) {
+    const name = req.body.name
+    const start = req.body.startDate
+    const end = req.body.endDate
+    const startDate = new Date(req.body.startDate)
+    const endDate = new Date(req.body.endDate)
+    const desc = req.body.desc
+    const checkbox = req.body.checkbox
+    const checkboxes = ['nodeJs', 'nextJs', 'reactJs', 'typeScript'].filter(checkbox => req.body[checkbox])
+    const id = req.body.id
+    
+    data[parseInt(id)] = {
+        name,
+        start,
+        end,
+        desc,
+        checkboxes
+    }
+
+    // Hitung selisih dalam hari
+    const days = dayDifference(startDate , endDate)
+
+    // Menentukan unit durasi berdasarkan selisih waktu
+    const { duration, unit } = chooseDuration(days)
+
+    // Ambil icon yang dipilih
+    const icons = {
+        nodeJs: checkboxes.includes('nodeJs') ? "/assets/img/nodejs.png" : '',
+        nextJs: checkboxes.includes('nextJs') ? "/assets/img/nextjs.png" : '',
+        reactJs: checkboxes.includes('reactJs') ? "/assets/img/react.png"  : '',
+        typeScript: checkboxes.includes('typeScript') ? "/assets/img/typescirpt.png" : ''
+    }
+
+    const dataProject = {name, start, end, startDate, endDate, desc, checkboxes, duration, unit, icons}
+
+    res.redirect('/')
+}
+
+function deleteCard(req, res) {
+    const id = req.params.id
+    
+    data.splice(id, 1)
     res.redirect('/')
 }
 
 function detail(req, res) {
-    res.render('detail')
+    const id = parseInt(req.params.id)
+
+    if(id >= 0 && id < data.length) {
+        const project = data[id]
+        res.render('detail', {data: project, start: project.start, end: project.end})
+    } else {
+        res.status(404).send('Project not found!')
+    }
 }
 
 function testimonials(req, res) {
     res.render('testimonial')
+}
+
+function dayDifference (start, end) {
+    const timeDiff = end.getTime() - start.getTime()
+    return Math.floor(timeDiff / (1000 * 60 * 60 * 24))
+}
+
+function chooseDuration(days) {
+    const years = Math.floor(days / 365)
+    const months = Math.floor((days % 365) / 30)
+    const remainingDays = days % 30
+
+    if(years > 0) {
+        return { duration: years, unit: 'tahun'}
+    } else if (months > 0) {
+        return { duration: months, unit: 'bulan'}
+    } else {
+        return { duration: remainingDays, unit: 'hari'}
+    }
 }
 
 app.listen(port, () => {
